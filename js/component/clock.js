6b1355db84e15b783f13418cb558d4cc45adb3d4
hhyukk <hhyukk1273@gmail.com>
2023-08-20 23:04:43 +0900

    clock

--- js/component/clock.js
+++ js/component/clock.js
@@ -11,15 +11,14 @@ function getClock() {
   } else {
     clockh1.innerText = `${hours}:${minutes}`;
   }
-  console.log(typeof Number(hours));
-  //   if (hours >= 06 && hours <= 11) {
-  //     greeting.innerText(`Good morning, ${localStorage.getItem("username")}.`);
-  //   }
-  //   if (hours >= 12 && hours <= 17) {
-  //     greeting.innerText(`Good afternoon, ${localStorage.getItem("username")}.`);
-  //   }
+
+  if (Number(hours) >= 6 && Number(hours) <= 11) {
+    greeting.innerText = `Good morning, ${localStorage.getItem("username")}.`;
+  }
+  if (Number(hours) >= 12 && Number(hours) <= 17) {
+    greeting.innerText = `Good afternoon, ${localStorage.getItem("username")}.`;
+  }
   if (Number(hours) >= 18 && Number(hours) <= 23) {
-    console.log(typeof Number(hours));
     greeting.innerText = `Good evening, ${localStorage.getItem("username")}.`;
   }
 }
